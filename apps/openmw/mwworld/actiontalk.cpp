e2cf811f00a5462b01c830a80d2fec12f39e533d
Skooma-Breath <110526221+Skooma-Breath@users.noreply.github.com>
2026-08-09 04:24:12 -0500

    fix(multiplayer): prevent combat arrest dialogue

--- apps/openmw/mwworld/actiontalk.cpp
+++ apps/openmw/mwworld/actiontalk.cpp
@@ -1,9 +1,21 @@
 #include "actiontalk.hpp"
 
+#ifdef BUILD_MULTIPLAYER
+#include <components/debug/debuglog.hpp>
+#endif
+
 #include "../mwbase/environment.hpp"
 #include "../mwbase/windowmanager.hpp"
 
 #include "../mwmechanics/actorutil.hpp"
+
+#ifdef BUILD_MULTIPLAYER
+#include "../mwbase/mechanicsmanager.hpp"
+#include "../mwmechanics/npcstats.hpp"
+#include "../mwmp/Main.hpp"
+
+#include "class.hpp"
+#endif
 
 namespace MWWorld
 {
@@ -14,7 +26,23 @@ namespace MWWorld
 
     void ActionTalk::executeImp(const Ptr& actor)
     {
-        if (actor == MWMechanics::getPlayer())
-            MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
+        if (actor != MWMechanics::getPlayer())
+            return;
+
+        const Ptr& target = getTarget();
+#ifdef BUILD_MULTIPLAYER
+        // Player-initiated guard dialogue bypasses AiPursue, so enforce the server's arrest policy here as well.
+        if (mwmp::Main::isInitialised()
+            && mwmp::Main::get().getGuardArrestMode() == mwmp::GuardArrestMode::Combat
+            && target.getClass().isNpc() && target.getClass().isClass(target, "Guard")
+            && actor.getClass().getNpcStats(actor).getBounty() > 0)
+        {
+            Log(Debug::Info) << "[MP] Guard dialogue converted to combat for " << target.toString();
+            MWBase::Environment::get().getMechanicsManager()->startCombat(target, actor, nullptr);
+            return;
+        }
+#endif
+
+        MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, target);
     }
 }
