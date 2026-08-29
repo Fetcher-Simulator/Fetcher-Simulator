8b0a76388655c0be135b5a51dbb578d98e4dc900
Skooma-Breath <110526221+Skooma-Breath@users.noreply.github.com>
2026-08-29 10:52:53 -0500

    fix(multiplayer): reduce startup restore stalls

--- apps/openmw/mwmp/Main.hpp
+++ apps/openmw/mwmp/Main.hpp
@@ -197,6 +197,7 @@ namespace mwmp
         std::unique_ptr<EnchantingCreationManager> mEnchantingCreationManager;
 
         std::string mPlayerName;
+        std::string mResolvedContentFingerprint;
         bool        mWorldReady           = false;
         bool        mIsNewCharacter       = true;
         bool        mHasSavedSpellbook     = false;
